6b56253dbd6d1f964c0234d33acca97ff57679d8
josenunoca <josenunoca@gmail.com>
2026-08-05 14:22:19 +0100

    feat(android): add runZonedGuarded safety wrapper and bump version to 1.0.2+3

--- lib/main.dart
+++ lib/main.dart
@@ -18,81 +18,88 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'config/app_config.dart';
+import 'dart:async';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.presentError(details);
-    debugPrint('Global FlutterError: ${details.exception}');
-  };
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+      debugPrint('Global FlutterError: ${details.exception}');
+    };
 
-  final localesToInit = [
-    'pt_PT', 'pt-PT',
-    'pt_BR', 'pt-BR',
-    'pt',
-    'en_US', 'en-US',
-    'en_GB', 'en-GB',
-    'en',
-    'es_ES', 'es-ES',
-    'es',
-    'fr_FR', 'fr-FR',
-    'fr',
-    'de_DE', 'de-DE',
-    'de',
-  ];
-  for (final loc in localesToInit) {
-    try {
-      await initializeDateFormatting(loc, null);
-    } catch (_) {
-      // Ignore unsupported or failing locales silently
+    final localesToInit = [
+      'pt_PT', 'pt-PT',
+      'pt_BR', 'pt-BR',
+      'pt',
+      'en_US', 'en-US',
+      'en_GB', 'en-GB',
+      'en',
+      'es_ES', 'es-ES',
+      'es',
+      'fr_FR', 'fr-FR',
+      'fr',
+      'de_DE', 'de-DE',
+      'de',
+    ];
+    for (final loc in localesToInit) {
+      try {
+        await initializeDateFormatting(loc, null);
+      } catch (_) {
+        // Ignore unsupported or failing locales silently
+      }
     }
-  }
-  Intl.defaultLocale = 'pt_PT';
+    Intl.defaultLocale = 'pt_PT';
 
-  try {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: AppConfig.firebaseApiKey,
-        authDomain: AppConfig.firebaseAuthDomain,
-        databaseURL: AppConfig.firebaseDatabaseURL,
-        projectId: AppConfig.firebaseProjectId,
-        storageBucket: AppConfig.firebaseStorageBucket,
-        messagingSenderId: AppConfig.firebaseMessagingSenderId,
-        appId: AppConfig.firebaseAppId,
+    try {
+      if (Firebase.apps.isEmpty) {
+        await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: AppConfig.firebaseApiKey,
+            authDomain: AppConfig.firebaseAuthDomain,
+            databaseURL: AppConfig.firebaseDatabaseURL,
+            projectId: AppConfig.firebaseProjectId,
+            storageBucket: AppConfig.firebaseStorageBucket,
+            messagingSenderId: AppConfig.firebaseMessagingSenderId,
+            appId: AppConfig.firebaseAppId,
+          ),
+        );
+      }
+    } catch (e) {
+      debugPrint('Firebase initialization error on startup: $e');
+    }
+
+    runApp(
+      MultiProvider(
+        providers: [
+          Provider<FirebaseService>(create: (_) => FirebaseService()),
+          ChangeNotifierProvider(create: (_) => LanguageProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+          ProxyProvider<LanguageProvider, AiTranslationService>(
+            update: (_, language, __) => AiTranslationService(AppConfig.geminiApiKey),
+          ),
+          Provider<AiChatService>(create: (_) => AiChatService(apiKey: AppConfig.geminiApiKey)),
+          Provider<LifestyleAiService>(create: (_) => LifestyleAiService(AppConfig.geminiApiKey)),
+          Provider<InstitutionalService>(create: (_) => InstitutionalService()),
+          ProxyProvider<FirebaseService, FinanceService>(
+            update: (_, firebase, __) => FinanceService(firebase),
+          ),
+          Provider<NotificationService>(create: (_) => NotificationService()),
+          ProxyProvider2<FirebaseService, NotificationService, ProcurementService>(
+            update: (_, firebase, notifications, __) => ProcurementService(firebase, notifications),
+          ),
+          Provider<InstitutionalKnowledgeService>(create: (_) => InstitutionalKnowledgeService()),
+          ProxyProvider<FirebaseService, DelegationService>(
+            update: (_, firebase, __) => DelegationService(firebase),
+          ),
+        ],
+        child: const EduGamingApp(),
       ),
     );
-  } catch (e) {
-    debugPrint('Firebase initialization error on startup: $e');
-  }
-
-  runApp(
-    MultiProvider(
-      providers: [
-        Provider<FirebaseService>(create: (_) => FirebaseService()),
-        ChangeNotifierProvider(create: (_) => LanguageProvider()),
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ProxyProvider<LanguageProvider, AiTranslationService>(
-          update: (_, language, __) => AiTranslationService(AppConfig.geminiApiKey),
-        ),
-        Provider<AiChatService>(create: (_) => AiChatService(apiKey: AppConfig.geminiApiKey)),
-        Provider<LifestyleAiService>(create: (_) => LifestyleAiService(AppConfig.geminiApiKey)),
-        Provider<InstitutionalService>(create: (_) => InstitutionalService()),
-        ProxyProvider<FirebaseService, FinanceService>(
-          update: (_, firebase, __) => FinanceService(firebase),
-        ),
-        Provider<NotificationService>(create: (_) => NotificationService()),
-        ProxyProvider2<FirebaseService, NotificationService, ProcurementService>(
-          update: (_, firebase, notifications, __) => ProcurementService(firebase, notifications),
-        ),
-        Provider<InstitutionalKnowledgeService>(create: (_) => InstitutionalKnowledgeService()),
-        ProxyProvider<FirebaseService, DelegationService>(
-          update: (_, firebase, __) => DelegationService(firebase),
-        ),
-      ],
-      child: const EduGamingApp(),
-    ),
-  );
+  }, (error, stack) {
+    debugPrint('Uncaught async error in runZonedGuarded: $error\n$stack');
+  });
 }
 
 class EduGamingApp extends StatelessWidget {
